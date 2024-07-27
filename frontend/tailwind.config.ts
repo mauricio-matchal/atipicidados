5459d7ea4105aef40bdd4e67ec7d0d7037f14d48
Stefanny <stefannypdao@gmail.com>
2024-07-27 00:40:33 -0300

    pasta frontend, primeiras config tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: '#000000',
+      blue: {
+        100: 'E5E7F6',
+        800: '27339A',
+        950: '191F54',
+      },
+      indigo: {
+        400: '7481F6',
+      },
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
