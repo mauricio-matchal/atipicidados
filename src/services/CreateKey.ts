c8ad83c6f6b05413bf7a58eced43856fcd1fef57
Isaac Silva Borges <isaacborges@ufba.br>
2024-07-28 20:53:40 -0300

    Adding first service

---
+++ src/services/CreateKey.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+
+//Generating a AES GCM encryption/decryption key for data securiity.
+class CreateKey {
+    async generateKey(): Promise<{  exportedKey: JsonWebKey }> {
+        try {
+            const newkey = await window.crypto.subtle.generateKey(
+                {
+                    name: 'AES-GCM',
+                    length: 256,
+                },
+                true,
+                ['encrypt', 'decrypt']
+            );
+
+            const exportedKey = await window.crypto.subtle.exportKey('jwk', newkey);
+            return { exportedKey }
+        } catch (err) {
+          console.error(err);
+          throw err;
+    }
+
+  }
+  
+}
+
+export { CreateKey };
+//Once the key is generated, it is exported as a Json Web Key
+//Here we're making it true for export and available for both encrypt and decrypt
+//Subtle and window gives access to web cryptography functions
