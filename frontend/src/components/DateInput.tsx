3752821567a0553c8331e26b34ef241699b9558a
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-08-15 21:01:45 -0300

    modified:   frontend/src/components/DateInput.tsx

--- frontend/src/components/DateInput.tsx
+++ frontend/src/components/DateInput.tsx
@@ -1,18 +1,24 @@
-import { JSX, ClassAttributes, InputHTMLAttributes } from "react";
+import { JSX, ClassAttributes, InputHTMLAttributes, useState } from "react";
 
-interface TextInputProps {
+interface DateInputProps {
   placeholder: string;
 }
 
-export default function TextInput({ placeholder, ...props }: JSX.IntrinsicAttributes & ClassAttributes<HTMLInputElement> & InputHTMLAttributes<HTMLInputElement>) {
+export default function DateInput({ placeholder, ...props }: JSX.IntrinsicAttributes & ClassAttributes<HTMLInputElement> & InputHTMLAttributes<HTMLInputElement>) {
+  const [hasValue, setHasValue] = useState(false);
 
   return (
-    <div className="w-full" {...props}>
-      <input
-        type="date"
-        className=" inline-block w-full py-[10px] leading-[19px] px-4 bg-transparent border-2 border-[#7481F6] border-opacity-70 rounded-xl focus:outline-none focus:border-blue-800"
-        placeholder={placeholder}
-      />
+    <div className="w-full relative" {...props}>
+
+      <div className="relative">
+        <input
+          type="date"
+          id="date-input"
+          className="w-full py-[10px] leading-[19px] h-[43px] px-3 bg-transparent border-2 border-[#7481F6] border-opacity-70 rounded-xl focus:outline-none focus:border-blue-800 appearance-none"
+          onChange={(e) => setHasValue(!!e.target.value)}
+          {...props}
+        />
+      </div>
     </div>
   )
 }
