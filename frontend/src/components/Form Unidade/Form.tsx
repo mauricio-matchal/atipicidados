22b77b46e13e3e91f1f6212a60a5c89626e1d55c
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-09-18 20:44:26 -0300

    modified:   frontend/src/components/Form Unidade/Form.tsx 	modified:   frontend/src/components/Form Unidade/Step1.tsx 	deleted:    frontend/src/components/Form Unidade/Step2.tsx 	deleted:    frontend/src/components/Form Unidade/Step3.tsx 	deleted:    frontend/src/components/Form Unidade/Step4.tsx

--- frontend/src/components/Form Unidade/Form.tsx
+++ frontend/src/components/Form Unidade/Form.tsx
@@ -1,62 +1,25 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
-import Step4 from './Step4';
 import { FormData } from './types';
 
 const Form: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [formData, setFormData] = useState({
-    geral: null,
-    escola: null,
-    mae: null,
-    pai: null,
-    maisinfo: null,
-    saudeinfo: null
+    nome: null,
+    endereco: null,
   });
 
   const nextStep = () => setCurrentStep(currentStep + 1);
   const prevStep = () => setCurrentStep(currentStep - 1);
 
-  const updateGeral = (data: any) => {
+  const updateForm = (data: any) => {
     setFormData((prevData) => ({
       ...prevData,
-      [`geral`]: data,
+      'nome': data.nome,
+      'endereco': data.endereco,
     }));
   };
-  const updateEscola = (data: any) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [`escola`]: data,
-    }));
-  };
-  const updateMae = (data: any) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [`mae`]: data,
-    }));
-  };
-  const updatePai = (data: any) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [`pai`]: data,
-    }));
-  };
-  const updateMaisInfo = (data: any) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [`maisinfo`]: data,
-    }));
-  };
-  const updateInfoSaude = (data: any) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [`saudeinfo`]: data,
-    }));
-  };
-
 
   const reveal = () => {
     console.log(formData);
@@ -81,32 +44,8 @@ const Form: React.FC = () => {
       return <>
         <button onClick={reveal}>reveal</button>
         <Step1
-          nextStep={nextStep}
-          updateGeral={(data) => updateGeral(data)}
-          updateEscola={(data) => updateEscola(data)}
-        />;
-      </>
-    case 2:
-      return <Step2
-        nextStep={nextStep}
-        prevStep={prevStep}
-        updateMae={(data) => updateMae(data)}
-        updatePai={(data) => updatePai(data)}
-      />;
-    case 3:
-      return <Step3
-        nextStep={nextStep}
-        prevStep={prevStep}
-        updateMaisInfo={(data) => updateMaisInfo(data)}
-      />;
-    case 4:
-      return <>
-        <button onClick={reveal}>reveal</button>
-        <button onClick={handleUserCreation}>rodar a rota</button>
-        <Step4
-          prevStep={prevStep}
-          updateInfoSaude={(data) => updateInfoSaude(data)}
           handleFormDataSubmit={handleUserCreation}
+          updateForm={(data) => updateForm(data)}
         />;
       </>
     default:
