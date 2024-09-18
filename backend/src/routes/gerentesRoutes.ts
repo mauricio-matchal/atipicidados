47ef460ddfbb1d541cefbb0cbb1ccb0eef8b45f0
Júlio César <juliomoreira@ufba.br>
2024-09-18 15:59:17 -0300

    atualizacoes de mauricio

--- backend/src/routes/gerentesRoutes.ts
+++ backend/src/routes/gerentesRoutes.ts
@@ -14,12 +14,9 @@ gerentesRouter.post('/', validateGerente(GerenteCreateInputSchema), createUserGe
 gerentesRouter.post('/buscar', getUserGerente);
 gerentesRouter.get('/:id', getUserGerenteId);
 gerentesRouter.post('/senha', sendPassword );
-<<<<<<<<< Temporary merge branch 1
 gerentesRouter.post('/login', gerenteLogin );
-=========
 
 
 
->>>>>>>>> Temporary merge branch 2
 
 export default gerentesRouter;
