96bcf28e75a12533647e9f2d9e6411097f81c689
Júlio César <juliomoreira@ufba.br>
2024-12-05 22:33:52 -0300

    feat: função blob para imagens do back

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,13 @@ import express from 'express';
 import { userRouter } from './routes/routes'; 
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
-
 import cookieParser from 'cookie-parser';
-
+import path from 'path'; 
 
 const port = Number(process.env.EXPRESS_PORT) || 3002;
 
-
 const app = express();
+
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000', 
@@ -18,9 +16,10 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
+app.use('/imagens', express.static('uploads/'));
 
 app.use(userRouter);
 
 app.listen(port, () => {
-    console.log(`Está rodando na porta  ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
